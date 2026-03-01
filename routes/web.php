0709fb92a8b60ee867b9d8ccc73c1338db8b90b2
rahmanlana <rahmanlana044@gmail.com>
2026-03-01 19:11:31 +0700

    Praktikum - 4

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/level', [LevelController::class, 'index']);
